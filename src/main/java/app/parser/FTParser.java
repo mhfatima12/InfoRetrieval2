cee7bc29d965b19cd4d42dd0007c4f42c66f08ba
hritika2708 <43510108+hritika2708@users.noreply.github.com>
2024-11-16 00:27:43 +0000

    Update FTParser.java

--- src/main/java/app/parser/FTParser.java
+++ src/main/java/app/parser/FTParser.java
@@ -7,65 +7,74 @@ import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import app.model.childModel.FTModel;
+import org.apache.lucene.index.IndexWriter;
 
 public class FTParser {
 
-//    private final static File FT_DIR = new File("docs/ft");
-    private static ArrayList<FTModel> ftDocsList = new ArrayList<>();
-    private List<Document> ftDoc = new ArrayList<>();
-
-    private List<Document> doclist;
-
-    public List<Document> parseFTDocs(String path) throws IOException {
-        doclist = new ArrayList<>();
-        System.out.println("Parsing FT documents...");
+    /**
+     * Parses FT documents and directly indexes them to reduce memory usage.
+     * @param path The directory containing the FT documents.
+     * @param writer The IndexWriter to add documents to the index.
+     * @throws IOException if an I/O error occurs.
+     */
+    public void parseAndIndexFTDocs(String path, IndexWriter writer) throws IOException {
+        System.out.println("Parsing and indexing FT documents from path: " + path);
+        
         File[] directories = new File(path).listFiles(File::isDirectory);
-
-        for(File directory : directories){
+        
+        for (File directory : directories) {
             File[] files = directory.listFiles();
-            for(File file : files) {
+            for (File file : files) {
+                // Parse the document
                 org.jsoup.nodes.Document doc = Jsoup.parse(file, null, "");
                 Elements elements = doc.select("DOC");
-                for(Element element : elements){
-                    FTModel ftdoc = new FTModel();
 
-                    String headlineText = element.select("HEADLINE").text();
-//                    String[] arrOfStr = str.split("/", 2);
-//                    ftdoc.setDate(arrOfStr[0].split(" ", 2)[1]);
-                    ftdoc.setTitle(headlineText);
-                    ftdoc.setDocNo(element.select("DOCNO").text());
-                    ftdoc.setContent(element.select("TEXT").text());
-
-                    ftDocsList.add(ftdoc);
+                // Process each element and index immediately
+                for (Element element : elements) {
+                    FTModel ftDoc = parseFTDocument(element);
+                    Document luceneDoc = createLuceneDocument(ftDoc);
+                    
+                    // Index the document immediately
+                    writer.addDocument(luceneDoc);
                 }
             }
         }
-        System.out.println("Parsing FT done...");
-        return returnParsedDocuments();
+        
+        System.out.println("Indexing process completed.");
     }
 
-    public List<Document> returnParsedDocuments() throws IOException{
-
-        for (FTModel ftModel : ftDocsList) {
-            Document document = new Document();
-            document.add(new StringField("docNumber", ftModel.getDocNo(), Field.Store.YES));
-            FieldType fieldType = new FieldType(TextField.TYPE_STORED);
-            fieldType.setStoreTermVectors(true);
-//            document.add(new TextField("docTitle", ftModel.getTitle(), Field.Store.YES));
-//            document.add(new TextField("docContent", ftModel.getContent(), Field.Store.YES));
-            document.add(new Field("docTitle", ftModel.getTitle(), fieldType));
-            document.add(new Field("docContent", ftModel.getContent(), fieldType));
-            ftDoc.add(document);
-        }
-
+    /**
+     * Parses the FT document from the element.
+     * @param element The document element.
+     * @return The parsed FTModel object.
+     */
+    private FTModel parseFTDocument(Element element) {
+        FTModel ftDoc = new FTModel();
+        ftDoc.setTitle(element.select("HEADLINE").text());
+        ftDoc.setDocNo(element.select("DOCNO").text());
+        ftDoc.setContent(element.select("TEXT").text());
         return ftDoc;
     }
 
-    public static void main(String[] args) throws IOException {
-        new FTParser().parseFTDocs("docs/ft");
+    /**
+     * Converts FTModel to a Lucene Document.
+     * @param ftModel The FTModel object containing document data.
+     * @return The corresponding Lucene Document.
+     */
+    private Document createLuceneDocument(FTModel ftModel) {
+        Document document = new Document();
+        document.add(new StringField("docNumber", ftModel.getDocNo(), Field.Store.YES));
+
+        // Use a FieldType with term vectors for storing text and its analysis
+        FieldType fieldType = new FieldType(TextField.TYPE_STORED);
+        fieldType.setStoreTermVectors(true);
+
+        document.add(new Field("docTitle", ftModel.getTitle(), fieldType));
+        document.add(new Field("docContent", ftModel.getContent(), fieldType));
+
+        return document;
     }
+
 }
